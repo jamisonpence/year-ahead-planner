5e71e331804b0ca42be289870fc38dce0dc3cdbc
jamisonpence <jamisonpence@gmail.com>
2026-04-18 07:19:53 -0500

    Redirect unauthenticated catch-all to landing page

--- server/static.ts
+++ server/static.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 export function serveStatic(app: Express) {
   const distPath = path.resolve(__dirname, "public");
+
   if (!fs.existsSync(distPath)) {
     throw new Error(
       `Could not find the build directory: ${distPath}, make sure to build the client first`,
@@ -13,7 +14,8 @@ export function serveStatic(app: Express) {
   app.use(express.static(distPath));
 
   // fall through to index.html if the file doesn't exist
-  app.use("/{*path}", (_req, res) => {
+  app.use("/{*path}", (req: any, res) => {
+    if (!req.isAuthenticated()) return res.redirect("/");
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
